4aee52baad132aee00522d8f42577f6cf91f0369
Yuri Zicatti <yuri.zkt@gmail.com>
2026-04-09 19:40:45 -0300

    feat: selecionando refeicoes por categoria

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import '../models/category.dart';
+import '../data/dummy_data.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)?.settings.arguments as Category;
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(title: Text(category.title)),
-      body: Center(child: Text('Receitas na categoria ${category.id}')),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (context, index) {
+          return Text(categoryMeals[index].title);
+        },
+      ),
     );
   }
 }
