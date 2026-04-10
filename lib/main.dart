5ad75157e4b4ea8d4b1576491e074ab505ae9777
Yuri Zicatti <yuri.zkt@gmail.com>
2026-04-10 15:25:21 -0300

    feat: criando rota e tela de detalhes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 
 import 'utils/app_routes.dart';
 
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (_) => CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (_) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (_) => MealDetailScreen(),
       },
     );
   }
