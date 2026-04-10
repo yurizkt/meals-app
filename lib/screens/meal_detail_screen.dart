04ebc41d723423ddd872dad1db35349bf369dc47
Yuri Zicatti <yuri.zkt@gmail.com>
2026-04-10 16:26:44 -0300

    feat: tela de detalhes refeicao part 1

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -7,7 +7,51 @@ class MealDetailScreen extends StatelessWidget {
     final meal = ModalRoute.of(context)?.settings.arguments as Meal;
     return Scaffold(
       appBar: AppBar(title: Text(meal.title)),
-      body: Center(child: Text('Detalhes da refeição')),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(meal.imageUrl, fit: BoxFit.cover),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredientes: ',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          Container(
+            width: 200,
+            height: 300,
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.all(6),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: ListView.builder(
+              itemCount: meal.ingredients.length,
+              itemBuilder: (ctx, index) {
+                return Card(
+                  color: Theme.of(context).primaryColor,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 6,
+                      horizontal: 12,
+                    ),
+                    child: Text(
+                      meal.ingredients[index],
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
